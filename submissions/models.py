fac5a1ec4588d57501019a0f8040b8fca20efe2b
Ruppy <rupeshk7778@gmail.com>
2023-08-01 16:46:04 +0530

    Lil changes to authentication and templates

--- submissions/models.py
+++ submissions/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+SUBMISSION_TYPES = (
+    ('image', 'Image'),
+    ('file', 'File'),
+    ('link', 'Link'),
+)
--- submissions/views.py
+++ submissions/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.password_validation import validate_password
 
 from .serializers import UserSerializer
 from rest_framework import status
@@ -16,30 +17,47 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@api_view(['POST'])
+@api_view(['GET','POST'])
 def userLogin(request):
-    
-    username = request.data.get('username')
-    password = request.data.get('password')
-    
-    user = authenticate(request, username= username, password=password)
-    if user is not None:
-        login(request, user)
-        return redirect("home")
-    else:
-        return Response({"Error" : "Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+    if request.method == "POST":
+        username = request.data.get('username')
+        password = request.data.get('password')
 
-@api_view(['POST'])
+        user = authenticate(request, username= username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            return Response({"Error" : "Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+    elif request.method == "GET":
+        return render(request, 'index.html')
+
+@api_view(['GET', 'POST'])
 def signup(request):
-    serializer = UserSerializer(data = request.data)
-    if serializer.is_valid():
-        serializer.save()
-        user = User.objects.get(username=request.data['username'])
-        user.set_password(request.data['password'])
-        user.save()
-        token = Token.objects.create(user=user)
-        return Response({"token" : token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            username = serializer.validated_data.get('username')
+            password = serializer.validated_data.get('password')
+            email = serializer.validated_data.get('email')
+            if User.objects.filter(username=username).exists():
+                error = "Username is already taken."
+            elif User.objects.filter(email=email).exists():
+                error = "Email is already in use!"
+            else:
+                try:
+                    validate_password(password)
+                    user = User.objects.create_user(username=username, password=password, email=email)
+                    token, created = Token.objects.get_or_create(user=user)
+                    login(request, user)
+                    return redirect("home")
+                except Exception as e:
+                    error = str(e)
+        else:
+            error = "Invalid data. Please check the form fields."
+        return render(request, 'signup.html', {'error': error})
+    elif request.method == 'GET':
+        return render(request, 'signup.html')
 
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
